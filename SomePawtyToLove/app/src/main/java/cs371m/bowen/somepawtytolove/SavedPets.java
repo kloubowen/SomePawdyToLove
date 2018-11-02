cb766596dfccea3fe3d5a25c0e07a4736034fa88
cgaitan1 <35742363+cgaitan1@users.noreply.github.com>
2018-11-01 21:43:55 -0500

    Added more to PetAdapter

--- SomePawtyToLove/app/src/main/java/cs371m/bowen/somepawtytolove/SavedPets.java
+++ SomePawtyToLove/app/src/main/java/cs371m/bowen/somepawtytolove/SavedPets.java
@@ -1,13 +1,43 @@
 package cs371m.bowen.somepawtytolove;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SavedPets extends AppCompatActivity {
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private List<Pet> pets;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.saved_pets);
+        recyclerView = (RecyclerView) findViewById(R.id.savedPets);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setRefreshing(false);
+        swipeRefreshLayout.setEnabled(false);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        pets = new ArrayList<>();
+        // retrieve saved pets from somewhere
+        PetAdapter petAdapter = new PetAdapter(this, pets);
+        recyclerView.setAdapter(petAdapter);
     }
+
+    public void addPet(Pet newPet){
+        pets.add(0, newPet);
+        recyclerView.getAdapter().notifyItemInserted(0);
+    }
+
+    // figure out how to update adapter if this activity hasn't been opened yet.
+    // maybe have calling activity pass in a list of new pets
+    // maybe have a static saved pet list in main activity
 }
