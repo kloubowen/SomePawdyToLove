efbcc758d4152afa89040e773ccf9545049e66fc
kloubowen <klbowen122@gmail.com>
2018-11-28 12:23:34 -0600

    Added users

--- SomePawtyToLove/app/src/main/java/cs371m/bowen/somepawtytolove/MainActivity.java
+++ SomePawtyToLove/app/src/main/java/cs371m/bowen/somepawtytolove/MainActivity.java
@@ -9,12 +9,19 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -29,11 +36,20 @@ public class MainActivity extends AppCompatActivity implements PetJson.IPetJson 
     private Net net;
     private Pet currentPet;
     private FloatingActionButton rejectButton, saveButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser==null)
+            updateUser();
+        else
+            Log.i("authcheck", "current user: "+ currentUser.getEmail());
 
         //Set up listeners
         rejectButton = findViewById(R.id.floatingReject);
@@ -69,6 +85,21 @@ public class MainActivity extends AppCompatActivity implements PetJson.IPetJson 
         mySettings.put("Breed", null);
         mySettings.put("Sex", null);
     }
+
+    public void updateUser() {
+        Log.i("authcheck", "update user called");
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(), 1);
+
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
